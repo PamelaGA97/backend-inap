1e4e2ab65545740ba77064ee647ff12236ec0a4c
sergioJ92 <jeral.sergio@gmail.com>
2025-12-08 09:32:39 -0300

    save changes

--- src/modules/courses/entities/course.entity.ts
+++ src/modules/courses/entities/course.entity.ts
@@ -1,11 +1,15 @@
 import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
 import { BaseEntity } from "../../base/base.entity";
 import { Faculty } from "../../faculties/entities/faculty.entity";
+import { CareerTimeEnum } from "../enum/career-time.enum";
 
 @Entity()
 export class Course extends BaseEntity {
     @Column()
     name: string;
+
+    @Column()
+    courseTime: CareerTimeEnum;
 
     @ManyToOne(() => Faculty, (faculty) => faculty.courses, {onDelete: 'CASCADE'})
     faculty: Faculty;
